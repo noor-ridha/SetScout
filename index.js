7342a0d814cd5d78eb169969a9297d9db43ac921
noor <noorridha028@gmail.com>
2023-05-15 18:59:52 +0300

    connected to mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 //create the route
 // http method
@@ -11,8 +12,20 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   res.send("here is the post method");
 });
-const port = 3000;
-// the call back function will be called as soon as the server starts listening
-app.listen(port, () => {
-  console.log(`your application is running on port ${port}`);
-});
+
+// connect to mongoDB
+mongoose
+  .connect(
+    "mongodb+srv://noor:Adminnoor1000@cluster0.gxkmxat.mongodb.net/NodeApi?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected");
+    const port = 3000;
+    // the call back function will be called as soon as the server starts listening
+    app.listen(port, () => {
+      console.log(`your application is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
