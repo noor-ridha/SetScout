f969a7bd0f05ab84f4cde401b7fc47fec943b9fe
noor <noorridha028@gmail.com>
2023-06-23 15:39:49 +0300

    added locations

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,10 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import MovieBox from "../components/MovieBox.js";
-// import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const Home = () => {
-  // const [locations, setLocations] = useState([]);
   const [movies, setMovies] = useState([]);
   const data1 = useEffect(() => {
     const fetchMovie = async () => {
@@ -17,13 +15,6 @@ const Home = () => {
             const respo = res.data;
             setMovies(respo);
             // console.log(respo);
-            // console.log(respo[0].location);
-            // const updatedLocations = respo.map((movie) => ({
-            //   lat: movie.location[0],
-            //   lng: movie.location[1],
-            // }));
-
-            // setLocations(updatedLocations);
           });
       } catch (err) {
         console.log(err);
--- client/src/pages/SingleMovie.jsx
+++ client/src/pages/SingleMovie.jsx
@@ -1,11 +1,19 @@
+// SingleMovie.jsx
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import OneMovie from "../components/OneMovie.jsx";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "75%",
+  height: "300px",
+};
 
 const SingleMovie = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
+  const [locations, setLocations] = useState([]);
 
   useEffect(() => {
     const fetchMovie = async () => {
@@ -13,11 +21,16 @@ const SingleMovie = () => {
         const response = await axios.get(
           `http://localhost:3000/movies/${movieId}`
         );
-        // console.log(response);
-        const fetchedMovie = response.data;
-        // console.log(fetchedMovie);
+        const allData = response.data;
+        // console.log(allData);
+        setMovie(allData);
+        console.log(allData.location);
+        const updatedLocations = [allData.location].map((positions) => ({
+          lat: positions[0],
+          lng: positions[1],
+        }));
 
-        setMovie(fetchedMovie);
+        setLocations(updatedLocations);
       } catch (err) {
         console.log(err);
       }
@@ -29,10 +42,22 @@ const SingleMovie = () => {
   if (!movie) {
     return <div>Loading...</div>;
   }
+  const center =
+    locations.length > 0
+      ? locations[0]
+      : { lat: 41.11895981243429, lng: 28.999364428716643 };
 
   return (
     <div>
       <OneMovie {...movie} />
+      <LoadScript googleMapsApiKey="AIzaSyAZJGmm_7LJg1YGZPdtHeNKwUrz1FqKYmY">
+        {" "}
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={14}>
+          {locations.map((location, index) => (
+            <Marker key={index} position={location} />
+          ))}
+        </GoogleMap>
+      </LoadScript>
     </div>
   );
 };
