e3056ec880665434311fa318de2a442cdcdf3626
noor <noorridha028@gmail.com>
2023-06-26 21:12:30 +0300

    changes

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import set from "../imges/sett.png";
-
+import Set from "../imges/nav.png";
 const Nav = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-20 bg-orange-300  z-[100] flex justify-between">
-      <img src={set} height={50} width={125} alt="pic"></img>
+      <div className="flex items-center">
+        <img
+          src={Set}
+          style={{ height: "180px", width: "125px" }}
+          alt="pic"
+        ></img>
+      </div>
       <ul className="flex justify-end items-center w-full h-full px-2 text-lg">
         <li className="px-4">
           <Link to="/">Home</Link>
@@ -18,7 +23,7 @@ const Nav = () => {
           <Link to="/tv">Tv Shows</Link>
         </li>
         <li className="px-4">
-          <Link to="/about">About Us</Link>
+          <Link to="/about">About</Link>
         </li>
       </ul>
     </div>
