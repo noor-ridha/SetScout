f5b3885046a3179cc94948c5c0352cdd523662a4
noor <noorridha028@gmail.com>
2023-06-26 21:32:47 +0300

    styling

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Set from "../imges/nav.png";
+import Set from "../imges/Logo.png";
 const Nav = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-20 bg-orange-300  z-[100] flex justify-between">
       <div className="flex items-center">
         <img
           src={Set}
-          style={{ height: "180px", width: "125px" }}
+          style={{ height: "180px", width: "180px" }}
           alt="pic"
         ></img>
       </div>
       <ul className="flex justify-end items-center w-full h-full px-2 text-lg">
-        <li className="px-4">
+        <li className="px-4 text-cyan-800 font-semibold">
           <Link to="/">Home</Link>
         </li>
-        <li className="px-4">
+        <li className="px-4 text-cyan-800 font-semibold">
           {" "}
           <Link to="/movies">Movies</Link>
         </li>
-        <li className="px-4">
+        <li className="px-4 text-cyan-800 font-semibold">
           <Link to="/tv">Tv Shows</Link>
         </li>
-        <li className="px-4">
+        <li className="px-4 text-cyan-800 font-semibold">
           <Link to="/about">About</Link>
         </li>
       </ul>
