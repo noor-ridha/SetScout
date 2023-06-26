4e0626cc8c52cbdeed8d1222bf5e71f439f814c9
noor <noorridha028@gmail.com>
2023-06-26 14:04:24 +0300

    preper for production

--- client/src/pages/SingleMovie.jsx
+++ client/src/pages/SingleMovie.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import OneMovie from "../components/OneMovie.jsx";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-// import { config } from "dotenv";
+// import { googleKey } from "dotenv";
 // config();
 const containerStyle = {
   width: "75%",
